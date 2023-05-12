7f1faa19e9138e74f3744563960efca249922fef
Simone <ssimone12@gmail.com>
2023-05-12 09:43:10 +0200

    form skal slettes

--- booking-site/src/pages/_app.jsx
+++ booking-site/src/pages/_app.jsx
@@ -1,13 +1,6 @@
-import '@/styles/globals.css'
-import App from 'next/app'
+import "@/styles/globals.css";
+import App from "next/app";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
-
-
-MyApp.getInitialProps = async (appContext) => {
-  //provide appContext in order to do 404's
-  
-  }
-  
---
+++ booking-site/src/pages/form.jsx
@@ -0,0 +1,12 @@
+/* test */
+
+export default function Form() {
+  return (
+    <>
+      <h1>Tickets</h1>
+      <form action="">
+        <label htmlFor="ticket">ticket</label>
+      </form>
+    </>
+  );
+}
