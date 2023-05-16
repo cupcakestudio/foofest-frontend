d925b14d93832cb29cd39dc512560524dfe19f7e
Ting Hua Lin <coolting.lin3@gmail.com>
2023-05-16 11:16:24 +0200

    make ticket-type a dropdown field

--- booking-site/src/pages/form.jsx
+++ booking-site/src/pages/form.jsx
@@ -1,5 +1,16 @@
 import { useState } from "react";
-import { InputLabel, FormControl, Card, CardContent, TextField, Select, Checkbox, FormGroup, FormControlLabel, MenuItem } from "@mui/material";
+import {
+  InputLabel,
+  FormControl,
+  Card,
+  CardContent,
+  TextField,
+  Select,
+  Checkbox,
+  FormGroup,
+  FormControlLabel,
+  MenuItem,
+} from "@mui/material";
 import styles from "../styles/Form.module.css";
 
 export default function Form({ spots }) {
@@ -21,20 +32,60 @@ export default function Form({ spots }) {
               <h2>Tickets</h2>
               <TextField label="Date" />
               <TextField label="Tickets" />
-              <TextField label="Ticket Type" />
+
+              <InputLabel
+                id="dropdown-label"
+                label="ticket-type"
+                placeholder="Ticket-type"
+                className={styles.dropdownLabel}
+                style={{ position: "relative" }}
+              >
+                Choose Tickets
+              </InputLabel>
+              <Select
+                labelId="ticket-type"
+                id="dropdowm"
+                label="Ticket-Type"
+                value={(selectedSpot, selectedArea)}
+                onChange={handleChange}
+              >
+                <MenuItem value="reg">Regular</MenuItem>
+                <MenuItem value="vip">VIP</MenuItem>
+              </Select>
               <h2>Available Spots:</h2>
-              <FormGroup labelId="dropdown-label" id="dropdowm" label="Available spots" value={(selectedSpot, selectedArea)} onChange={handleChange}>
+              <FormGroup
+                labelId="dropdown-label"
+                id="dropdowm"
+                label="Available spots"
+                value={(selectedSpot, selectedArea)}
+                onChange={handleChange}
+              >
                 {spots.map((spot, availability) => (
                   <p key={availability} value={(spot.available, spot.area)}>
                     {spot.area + ": " + spot.available}
                   </p>
                 ))}
-                <InputLabel id="dropdown-label" label="spots" placeholder="form" className={styles.dropdownLabel} style={{ position: "relative" }}>
+                <InputLabel
+                  id="dropdown-label"
+                  label="spots"
+                  placeholder="form"
+                  className={styles.dropdownLabel}
+                  style={{ position: "relative" }}
+                >
                   Choose Camp
                 </InputLabel>
-                <Select labelId="dropdown-label" id="dropdowm" label="Available spots" value={(selectedSpot, selectedArea)} onChange={handleChange}>
+                <Select
+                  labelId="dropdown-label"
+                  id="dropdowm"
+                  label="Available spots"
+                  value={(selectedSpot, selectedArea)}
+                  onChange={handleChange}
+                >
                   {spots.map((spot, availability) => (
-                    <MenuItem key={availability} value={(spot.available, spot.area)}>
+                    <MenuItem
+                      key={availability}
+                      value={(spot.available, spot.area)}
+                    >
                       {spot.area}
                     </MenuItem>
                   ))}
