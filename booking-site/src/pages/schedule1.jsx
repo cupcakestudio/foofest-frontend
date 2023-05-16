1d7f82ac8fd3cb92be9fe22a2204dccdc6683490
Alberte Nielsenpwd <albe237d@stud.kea.dk>
2023-05-16 12:44:16 +0200

    dage sat ind i schedule

--- booking-site/src/pages/schedule1.jsx
+++ booking-site/src/pages/schedule1.jsx
@@ -5,12 +5,10 @@ import { useState } from "react";
 
 import * as React from "react";
 import { Paper, Table } from "@mui/material";
-
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
-import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
 
 const columns = [
@@ -25,14 +23,35 @@ const columns = [
   },
   {
     id: "size",
-    label: "Size\u00a0(km\u00b2)",
+    label: "14:00 - 16:00",
     minWidth: 170,
     align: "right",
     format: (value) => value.toLocaleString("en-US"),
   },
   {
     id: "density",
-    label: "Density",
+    label: "16:00 - 18:00",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toFixed(2),
+  },
+  {
+    id: "density",
+    label: "18:00 - 20:00",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toFixed(2),
+  },
+  {
+    id: "density",
+    label: "20:00 - 22:00",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toFixed(2),
+  },
+  {
+    id: "density",
+    label: "22:00 - 00:00",
     minWidth: 170,
     align: "right",
     format: (value) => value.toFixed(2),
@@ -44,25 +63,42 @@ function createData(name, code, population, size) {
   return { name, code, population, size, density };
 }
 
-const rows = [
-  createData("India", "IN", 1324171354, 3287263),
-  createData("China", "CN", 1403500365, 9596961),
-  createData("Italy", "IT", 60483973, 301340),
-  createData("United States", "US", 327167434, 9833520),
-  createData("Canada", "CA", 37602103, 9984670),
-  createData("Australia", "AU", 25475400, 7692024),
-  createData("Germany", "DE", 83019200, 357578),
-  createData("Ireland", "IE", 4857000, 70273),
-  createData("Mexico", "MX", 126577691, 1972550),
-  createData("Japan", "JP", 126317000, 377973),
-  createData("France", "FR", 67022000, 640679),
-  createData("United Kingdom", "GB", 67545757, 242495),
-  createData("Russia", "RU", 146793744, 17098246),
-  createData("Nigeria", "NG", 200962417, 923768),
-  createData("Brazil", "BR", 210147125, 8515767),
-];
+const rows = [createData("Midgard", "IN", 1324171354, 3287263), createData("Vanaheim", "CN", 1403500365, 9596961), createData("Jotunheim", "IT", 60483973, 301340)];
 
 export default function StickyHeadTable() {
+  const [day, setDay] = useState("Monday");
+  //filter for button days
+  function changeDay(event) {
+    if (event.target.value === "Monday") {
+      setDay("Monday");
+      console.log(day);
+    }
+    if (event.target.value === "Tuesday") {
+      setDay("Tuesday");
+      console.log(day);
+    }
+    if (event.target.value === "Wednesday") {
+      setDay("Wednesday");
+      console.log(day);
+    }
+    if (event.target.value === "Thursday") {
+      setDay("Thursday");
+      console.log(day);
+    }
+    if (event.target.value === "Friday") {
+      setDay("Friday");
+      console.log(day);
+    }
+    if (event.target.value === "Saturday") {
+      setDay("Saturday");
+      console.log(day);
+    }
+    if (event.target.value === "Sunday") {
+      setDay("Sunday");
+      console.log(day);
+    }
+  }
+
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
@@ -76,37 +112,66 @@ export default function StickyHeadTable() {
   };
 
   return (
-    <Paper sx={{ width: "100%", overflow: "hidden" }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell key={column.id} align={column.align} style={{ minWidth: column.minWidth }}>
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-              return (
-                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                  {columns.map((column) => {
-                    const value = row[column.id];
-                    return (
-                      <TableCell key={column.id} align={column.align}>
-                        {column.format && typeof value === "number" ? column.format(value) : value}
-                      </TableCell>
-                    );
-                  })}
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination rowsPerPageOptions={[10, 25, 100]} component="div" count={rows.length} rowsPerPage={rowsPerPage} page={page} onPageChange={handleChangePage} onRowsPerPageChange={handleChangeRowsPerPage} />
-    </Paper>
+    <>
+      <h1>Schedule</h1>
+      {/* schedule timetable */}
+      {/* BUTTONS TO CHOOSE DAYS */}
+      <div className={stylesSchedule.days}>
+        <button onClick={changeDay} value="Monday">
+          Monday
+        </button>
+        <button onClick={changeDay} value="Tuesday">
+          Tuesday
+        </button>
+        <button onClick={changeDay} value="Wednesday">
+          Wednesday
+        </button>
+        <button onClick={changeDay} value="Thursday">
+          Thursday
+        </button>
+        <button onClick={changeDay} value="Friday">
+          Friday
+        </button>
+        <button onClick={changeDay} value="Saturday">
+          Saturday
+        </button>
+        <button onClick={changeDay} value="Sunday">
+          Sunday
+        </button>
+      </div>
+      <p className={stylesSchedule.dayName}>{day}</p>
+
+      <Paper sx={{ width: "100%", overflow: "hidden" }}>
+        <TableContainer sx={{ maxHeight: 440 }}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell key={column.id} align={column.align} style={{ minWidth: column.minWidth }}>
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === "number" ? column.format(value) : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+    </>
   );
 }
